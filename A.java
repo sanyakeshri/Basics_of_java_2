61e46b9bcb55836b156eea310447d2e6a4a20676
Sanya keshri <keshrisanya621@gmail.com>
2026-06-07 02:54:10 +0530

    String

---
+++ A.java
@@ -0,0 +1,15 @@
+// Lec 39(What is String):
+class A
+{
+    public static void main(String[]args)
+    {
+        String name = new String();
+        // OR
+        // String name="Navin";
+        System.out.println(name);
+		System.out.println(name.hashCode());
+		System.out.println("hello "+name);
+		System.out.println(name.concat("reddy"));
+		
+	}
+}
