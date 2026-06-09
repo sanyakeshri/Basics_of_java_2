eaaad56ca89adf5b5eeb01702bb87a00369e9891
Sanya keshri <keshrisanya621@gmail.com>
2026-06-09 09:16:13 +0530

    Mutable&Immutable

---
+++ B.java
@@ -0,0 +1,16 @@
+// Lec 40(Mutable V/S Immutable String):
+class A
+{
+    public static void main(String[]args)
+    {
+		String name="navin";
+		name=name+"reddy";
+		System.out.println("hello"+name);
+		
+		String s1="Navin";
+		String s2="Navin";
+		
+		System.out.println(s1==s2);
+		
+	}
+}
